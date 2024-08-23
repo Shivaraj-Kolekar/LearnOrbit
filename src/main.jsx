a7aa18a515fa8d6d424d887d719db4cf82fa3dda
Shivraj <shivkolekar01@gmail.com>
2024-08-23 17:27:21 +0530

    made auth changes

--- src/main.jsx
+++ src/main.jsx
@@ -8,21 +8,21 @@ import {
   createRoutesFromElements,
   createBrowserRouter
 } from 'react-router-dom'
+import { NextUIProvider } from '@nextui-org/react'
 import { ClerkProvider } from '@clerk/clerk-react'
 import Layout from './Layout.jsx'
-import Error from './pages/Error.jsx'
 import { dark } from '@clerk/themes'
 import Roadmaps from './pages/Roadmaps.jsx'
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
-const CLERK_FRONTEND_API = import.meta.env.VITE_CLERK_FRONTEND_API
+const clerkFrontendApi = import.meta.env.VITE_CLERK_FRONTEND_API
 if (!PUBLISHABLE_KEY) {
   throw new Error('Missing Publishable Key')
 }
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<Layout />}>
-      <Route index element={<App />} />
+    <Route path='/' element={<Layout></Layout>}>
+      <Route path='/' element={<App />} />
       <Route path='/roadmaps' element={<Roadmaps />} />
       <Route path='*' element={<Error />} />
     </Route>
