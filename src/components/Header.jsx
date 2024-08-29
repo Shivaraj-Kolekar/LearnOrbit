1daeb1f0ea12fc677e9af0c9115fd60c8a295155
Shivraj <shivkolekar01@gmail.com>
2024-08-29 12:19:23 +0530

    added product hunt url

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -35,7 +35,6 @@ function Header () {
         <button className='md:hidden text-white' onClick={toggleMenu}>
           {isMenuOpen ? <X size={24} /> : <Menu size={24} />}
         </button>
-
         {/* Desktop menu */}
         <div className='hidden md:flex flex-row items-center space-x-4'>
           <NavItems />
@@ -55,11 +54,25 @@ function Header () {
 function NavItems ({ mobile = false }) {
   return (
     <>
-      <div className='flex justify-between gap-3 self-center md:flex-row flex-col '>
+      <div className='flex justify-between gap-3 align-middle self-center md:flex-row flex-col '>
+        <a
+          className='mr-2'
+          href='https://www.producthunt.com/posts/learnorbit?embed=true&utm_source=badge-featured&utm_medium=badge&utm_souce=badge-learnorbit'
+          target='_blank'
+        >
+          <img
+            src='https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=484688&theme=light'
+            alt='LearnOrbit - Navigate&#0032;Your&#0032;Learning&#0032;Journey&#0032;with&#0032;Ease&#0032;&#0033; | Product Hunt'
+            style={{ width: '150px', height: '36px' }}
+            width='250'
+            height='40'
+          />
+        </a>
+
         <Link
           to='/roadmaps'
-          className={`inline-flex items-center text-gray-400 self-start md:self-center hover:text-white text-lg  font-semibold justify-center whitespace-nowrap rounded-md   ring-offset-background border-gray-700 transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50  border-input     ${
-            mobile ? 'block mb-2 text-center' : ''
+          className={`inline-flex items-center mx-0 text-gray-400 self-start md:self-center hover:text-white text-lg  font-semibold justify-center whitespace-nowrap rounded-md   ring-offset-background border-gray-700 transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50  border-input     ${
+            mobile ? 'block mb-0 text-center' : ''
           }`}
         >
           Roadmaps
